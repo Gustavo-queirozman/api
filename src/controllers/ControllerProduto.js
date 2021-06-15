e99b33abc183c1d528047d6ce0907ac2ce8aac87
Gustavo-queirozman <73914910+Gustavo-queirozman@users.noreply.github.com>
2021-06-15 09:19:19 -0300

    Update ControllerProduto.js

--- src/controllers/ControllerProduto.js
+++ src/controllers/ControllerProduto.js
@@ -1,1 +1,51 @@
+const Service = require('../services/ServiceProduto');
 
+module.exports = {
+    
+    buscarTodosProdutos: async (req, res) => {
+        let json = {result:[]};
+
+        let produto = await Service.buscarTodosProdutos();
+
+        for(let i in produto){
+            json.result.push({
+                id: produto[i].id,
+                nome: produto[i].nome,
+                preco: produto[i].preco,
+                validade: produto[i].validade,
+                foto: produto[i].foto,
+                categoria: produto[i].categoria,
+                quantidade: produto[i].quantidade,
+                lucro_unitario: produto[i].lucro_unitario
+            });
+        }
+
+        res.json(json);
+    },
+
+    buscarProduto: async (req, res) => {
+        let json = {result:{}};
+
+        let nome = req.query.nome; //para pegar o parametro
+        let produto = await Service.buscarProduto(nome);
+
+        if(produto){
+            json.result = produto; //se produto ele joga no json
+        }
+
+        res.json(json);
+    },
+
+    buscarCategoria: async (req, res) => {
+        let json = {result:{}};
+
+        let categoria = req.query.categoria; //para pegar o parametro
+        let produto = await Service.buscarCategoria(categoria);
+
+        if(produto){
+            json.result = produto; //se produto ele joga no json
+        }
+
+        res.json(json);
+    },
+}
