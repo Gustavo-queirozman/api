923894682417ccbc8df58e04648d502b76045f45
Gustavo-queirozman <73914910+Gustavo-queirozman@users.noreply.github.com>
2021-06-15 09:27:25 -0300

    Create ServiceProduto.js

---
+++ src/services/ServiceProduto.js
@@ -0,0 +1,42 @@
+const db = require('../db');
+
+module.exports = {
+
+    buscarTodosProdutos: () => {
+        return new Promise((aceito, rejeitado)=>{
+
+            db.query('SELECT * FROM estoque', (error, results)=>{
+                if(error) { rejeitado(error); return; }
+                aceito(results);
+            });
+        });
+    },
+
+    buscarProduto: (nome) => {
+        return new Promise((aceito, rejeitado)=>{
+
+            db.query('SELECT * FROM estoque WHERE nome = ?', [nome], (error, results) => {
+                if(error) { rejeitado(error); return; }
+                if(results.length > 0){ //vai retornar produtos com o valor da variável 'nome'
+                    aceito(results);
+                }else {
+                    aceito(false);
+                }
+            });
+        });
+    },
+
+    buscarCategoria: (categoria) => {
+        return new Promise((aceito, rejeitado)=>{
+
+            db.query('SELECT * FROM estoque WHERE categoria = ?', [categoria], (error, results) => {
+                if(error) { rejeitado(error); return; }
+                if(results.length > 0){ //vai retornar produtos com o valor da variável 'categoria'
+                    aceito(results);
+                }else {
+                    aceito(false);
+                }
+            });
+        });
+    },
+};
