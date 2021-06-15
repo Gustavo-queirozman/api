f9c322b0fa2e70605bf89e43988f7a67df4f61f4
ALVARO-UNIATENAS <83676336+ALVARO-UNIATENAS@users.noreply.github.com>
2021-06-14 23:22:23 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -23,9 +23,8 @@ server.use((req, res, next)=>{
     
 });
 
-
-server.use(express.json())
 server.use(express.urlencoded({ extended: true}))
+server.use(express.json())
 server.use('/', routes);
 
 server.listen(process.env.PORT,()=>{
